440e4e29ccaf7fe23dbe8d3285f61c8b6147fb33
sapphi-red <green@sapphi.red>
2020-09-01 16:31:15 +0900

    [avoidaccesslen] Add test

--- avoidaccesslen/testdata/src/a/a.go
+++ avoidaccesslen/testdata/src/a/a.go
@@ -5,4 +5,10 @@ import "fmt"
 func f() {
 	arr := []string{}
 	fmt.Println(arr[len(arr)]) // want "Likely cause index out of range"
+
+	arr2 := arr
+	fmt.Println(arr2[len(arr2)]) // want "Likely cause index out of range"
+
+	arr3 := []string{}
+	fmt.Println(arr3[len(arr)])
 }
